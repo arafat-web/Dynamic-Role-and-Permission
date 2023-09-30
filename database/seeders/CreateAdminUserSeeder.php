55ac51c3a3c858da974ba2250066c6dc7303d93c
Arafat Hossain Ar <arafat.122260@gmail.com>
2023-09-30 19:01:33 +0600

    update CreateAdminUserSeeder.php

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -7,46 +7,20 @@ use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
 
-  
-
 class CreateAdminUserSeeder extends Seeder
 
 {
 
-    /**
-
-     * Run the database seeds.
-
-     */
-
     public function run(): void
-
     {
-
         $user = User::create([
-
             'name' => 'Arafat', 
-
             'email' => 'admin@gmail.com',
-
             'password' => bcrypt('12345678')
-
         ]);
-
-        
-
         $role = Role::create(['name' => 'Admin']);
-
-         
-
         $permissions = Permission::pluck('id','id')->all();
-
-       
-
         $role->syncPermissions($permissions);
-
-         
-
         $user->assignRole([$role->id]);
 
     }
